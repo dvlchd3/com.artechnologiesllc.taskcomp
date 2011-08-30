cd51457349eb581a52e229dee1f2752c4d3b6a38
Dan Buhrman <dan.buhrman@gmail.com>
2011-08-30 00:33:01 -0400

    Problem DAO complete.

---
+++ TaskCompleter/src/main/java/com/artechnologiesllc/tc/persistence/dao/ProblemDAO.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.artechnologiesllc.tc.persistence.dao;
+
+import com.artechnologiesllc.tc.domain.Problem;
+
+/**
+ * Provides persistence operations for the <code>Problem</code> object.
+ * 
+ * @author Dan Buhrman
+ * 
+ * @see com.artechnologiesllc.taskcompleter.domain.Problem
+ */
+public interface ProblemDAO {
+    
+    /**
+     * Persists a <code>Problem</code> instance
+     * (create or update) depending on the value
+     * of the id.
+     * @param problem <code>Problem</code> to persist.
+     * @return Persisted instance.
+     */
+    public Problem persist(Problem problem);
+    
+    /**
+     * Removes a <code>Problem</code> from the database.
+     * @param problem <code>Problem</code> to remove.
+     */
+    public void delete(Problem problem);
+    
+    /**
+     * Retrieves a <code>Problem</code> from the database
+     * by its unique id.
+     * @param problemid Unique id of the <code>Problem</code>
+     *                  to retrieve.
+     * @return <code>Problem</code> instance from the database or
+     *          <code>null</code> if id cannot be found.
+     */
+    public Problem getProblem(Long problemid);
+}
---
+++ TaskCompleter/src/main/java/com/artechnologiesllc/tc/persistence/dao/hibernate/ProblemHibernateDAO.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.artechnologiesllc.tc.persistence.dao.hibernate;
+
+import com.artechnologiesllc.tc.domain.Problem;
+import com.artechnologiesllc.tc.persistence.dao.ProblemDAO;
+
+import java.util.List;
+import org.apache.log4j.Logger;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+/**
+ *
+ * @author dan
+ */
+public class ProblemHibernateDAO extends HibernateDaoSupport implements ProblemDAO {
+    
+    private static final Logger logger = Logger.getLogger(ProblemHibernateDAO.class);
+
+    @Override
+    public Problem persist(Problem problem) {
+        logger.debug("Persisting " + problem.toString());
+        
+        return (Problem) getHibernateTemplate().merge(problem);
+    }
+
+    public void delete(Problem problem) {
+        logger.debug("Deleting " + problem.toString());
+        getHibernateTemplate().delete(problem);
+    }
+
+    public Problem getProblem(Long problemid) {
+        logger.debug("Finding problem by id: " + (long)problemid);
+        
+        List result = getHibernateTemplate().findByNamedQuery("com.artechnologiesllc.tc.problemById", problemid);
+        if(result.size() <= 0) {
+            logger.debug("No match found.");
+            return null;
+        } else {
+            logger.debug("Match found.");
+            return (Problem)result.get(0);
+        }
+    }
+    
+}
