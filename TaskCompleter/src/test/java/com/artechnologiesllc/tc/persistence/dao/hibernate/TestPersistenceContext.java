7be085fb50db0c97c2f2ae6d3c2b23d51f623aea
Dan Buhrman <dan.buhrman@gmail.com>
2011-09-14 07:42:03 -0400

    Created Spring context file and class for testing.

---
+++ TaskCompleter/src/test/java/com/artechnologiesllc/tc/persistence/dao/hibernate/TestPersistenceContext.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.artechnologiesllc.tc.persistence.dao.hibernate;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author dan
+ */
+public class TestPersistenceContext extends ClassPathXmlApplicationContext {
+    
+    private static final String FILE_NAME = "testPersistenceContext.xml";
+    private static ApplicationContext instance;
+    
+    protected TestPersistenceContext(){
+        super(FILE_NAME);
+    }
+    
+    public static ApplicationContext getInstance() {
+        if(instance == null)
+            instance = new TestPersistenceContext();
+        
+        return instance;
+    }
+}
